6d077ece1d6b2753edf61a42bc676bc8c405e18c
dedeheri <dedeherii60@gmail.com>
2022-02-21 09:46:58 +0700

    bug fixed

--- src/Content/Store.js
+++ src/Content/Store.js
@@ -4,9 +4,6 @@ import thunk from "redux-thunk";
 // store
 import combineReducer from "./Reducer";
 
-const store = createStore(
-  combineReducer,
-  compose(applyMiddleware(thunk), window?.__REDUX_DEVTOOLS_EXTENSION__?.())
-);
+const store = createStore(combineReducer, compose(applyMiddleware(thunk)));
 
 export default store;
